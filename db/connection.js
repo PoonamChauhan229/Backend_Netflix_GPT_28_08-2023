aa52c5e841bc4be000ce8a753a82ed02b6d4cb65
Poonam229 <poonam.chauhan229@gmail.com>
2023-10-05 14:23:02 +0530

    just added

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
+const { default: axios } = require('axios')
 const mongoose=require('mongoose')
-
-
 const connection=async()=>{
     await mongoose.connect(process.env.MONGO_URL,{
         useNewUrlParser:true
